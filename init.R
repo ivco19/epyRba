e153e696d9e390a3f078e5e6fc44a5dbbf98c53c
Juan B Cabral <jbc.develop@gmail.com>
2020-04-03 20:31:42 -0300

    R version

---
+++ init.R
@@ -0,0 +1,10 @@
+## Install R packages
+packages <- c(“dplyr”, “devtools”)
+
+install_if_missing <- function(p) {
+    if (!p %in% rownames(installed.packages())) {
+        install.packages(p)
+    }
+}
+
+invisible(sapply(packages, install_if_missing))
