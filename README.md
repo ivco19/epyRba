51620fe0c92c6dbeda0c3792b93e01fca950a8b0
Juan B Cabral <jbc.develop@gmail.com>
2020-04-04 03:15:59 -0300

    fixes

--- README.md
+++ README.md
@@ -1,11 +1,50 @@
 # EPyRBa - Epicalc Python and R Backend
 
 
-## Notes
+Backend and mini-fronend for Epicalc in R
 
-### Deplyment
+## Installation
 
-- run `$heroku create`
-- Install R aside Python in heroku
-  https://www.r-bloggers.com/running-an-r-script-on-heroku/
-- git push heroku master
+    $ pip install -r requirements.txt
+    $ Rscript init.R
+    $ export FLASK_APP=epyrba
+    $ flask run --port 5001
+
+
+
+And then open your webbrowser in `http://localhost:5001`
+
+## Deplyment
+
+-   run $heroku create
+-   Install R aside Python in heroku <https://www.r-bloggers.com/running-an-r-script-on-heroku/>
+-   git push heroku master
+
+
+## Authors
+
+-   Juan B Cabral (CIFASIS-UNR, IATE-OAC-UNC).
+-   Mauricio Koraj (Liricus SRL.).
+-   Vanessa Daza (IATE-OAC-UNC, FaMAF-UNC).
+-   Mariano Dominguez (IATE-OAC-UNC, FaMAF-UNC).
+-   Marcelo Lares (IATE-OAC-UNC, FaMAF-UNC).
+-   Nadia Luczywo (LIMI-FCEFyN-UNC, IED-FCE-UNC, FCA-IUA-UNDEF)
+-   Dante Paz (IATE-OAC-UNC, FaMAF-UNC).
+-   Rodrigo Quiroga (INFIQC-CFQ, FCQ-UNC).
+-   Martín de los Ríos (ICTP-SAIFR).
+-   Bruno Sanchez (Department of Physics, Duke University).
+-   Federico Stasyszyn (IATE-OAC, FaMAF-UNC).
+
+
+## Afiliations:
+
+-   [Centro Franco Argentino de Ciencias de la Información y de Sistemas (CIFASIS-UNR)](https://www.cifasis-conicet.gov.ar/)
+-   [Instituto de Astronomía Téorico y Experimental (IATE-OAC-UNC)](http://iate.oac.uncor.edu/)
+-   [Facultad de Matemática Física y Computación (FaMAF-UNC)](https://www.famaf.unc.edu.ar/)
+-   [Laboratorio de Ingeniería y Mantenimiento Industrial (LIMI-FCEFyN-UNC)](https://fcefyn.unc.edu.ar/facultad/secretarias/investigacion-y-posgrado/-investigacion/laboratorio-de-ingenieria-y-mantenimiento-industrial/)
+-   [Instituto De Estadística Y Demografía - Facultad de Ciencias Económicas (IED-FCE-UNC)](http://www.eco.unc.edu.ar/instituto-de-estadistica-y-demografia)
+-   [Department of Physics, Duke University](https://phy.duke.edu/)
+-   [Facultad de Ciencias de la Administación (FCA-IUA-UNDEF)](https://www.iua.edu.ar/)
+-   [Instituto de Investigaciones en Físico-Química de Córdoba (INFIQC-CONICET)](http://infiqc-fcq.psi.unc.edu.ar/)
+-   [Liricus SRL](http://www.liricus.com.ar/)
+-   [ICTP South American Institute for Fundamental Research (ICTP-SAIFR)](ICTP-SAIFR)
--- epyrba.py
+++ epyrba.py
@@ -1,18 +1,17 @@
 import os
 import datetime as dt
-from io import StringIO
 import hashlib
 import json
 
 import sh
 
-import pandas as pd
+import jsonschema
 
 from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
 
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
 
 
 # =============================================================================
@@ -24,11 +23,12 @@ PRODUCTION = os.environ.get("EPYRBA_PRODUCTION", "").lower() == "true"
 
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 
+TEMPLATES_AUTO_RELOAD = PRODUCTION
 
 # The playhouse.flask_utils.FlaskDB object accepts database URL configuration.
 DATABASE = (
     os.environ["EPYRBA_DATABASE"] if PRODUCTION else
-    'sqliteext:///%s' % os.path.join(APP_DIR, 'epyrba.db'))
+    'sqliteext:///%s' % os.path.join(APP_DIR, '_cache_epyrba.db'))
 
 # if debug
 DEBUG = not PRODUCTION
@@ -48,9 +48,72 @@ app.config.from_object(__name__)
 # hooks for managing database connections.
 flask_db = FlaskDB(app)
 
-
 TTL = int(os.environ.get("EPYRBA_TTL", 60))
 
+
+# =============================================================================
+# JSON SCHEMA_
+# =============================================================================
+
+# check https://json-schema.org/
+SCHEMA = {
+    "type" : "object",
+    "properties" : {
+        'Time_to_death': {"type" : "number"},
+        'fact_futuro': {"type" : "number"},
+        'D_incbation': {"type" : "number"},
+        'D_infectious': {"type" : "number"},
+        'R0': {"type" : "number"},
+        'R0p': {"type" : "number"},
+        'D_recovery_mild': {"type" : "number"},
+        'D_recovery_severe': {"type" : "number"},
+        'D_hospital_lag': {"type" : "number"},
+        'retardo': {"type" : "number"},
+        'D_death': {"type" : "number"},
+        'p_fatal': {"type" : "number"},
+        'InterventionTime': {"type" : "number"},
+        'InterventionAmt': {"type" : "number"},
+        'p_severe': {"type" : "number"},
+        "E0": {"type" : "number"},
+        'duration': {"type" : "number"},
+        'N': {"type" : "number"},
+        'I0': {"type" : "number"},
+        'timepoints': {
+            "type": "array",
+            "minItems": 1,
+            "items": {"type": "number"}}
+    }
+}
+
+
+DEFAULT_PAYLOAD = """
+{
+  "Time_to_death": 17,
+  "fact_futuro": 0.5,
+  "D_incbation": 5.2,
+  "D_infectious": 2.9,
+  "R0": 3.422,
+  "R0p": 3.422,
+  "D_recovery_mild": 5.1,
+  "D_recovery_severe": 10.1,
+  "D_hospital_lag": 5,
+  "retardo": 4,
+  "D_death": 14.1,
+  "p_fatal": 0.021,
+  "InterventionTime": 18,
+  "InterventionAmt": 1.0,
+  "p_severe": 0.2,
+  "E0": 17.0,
+  "duration": 30,
+  "N": 440000.0,
+  "I0": 1,
+  "timepoints": [0, 1, 2, 3, 4]
+}""".strip()
+
+
+# =============================================================================
+# R COMMANDS
+# =============================================================================
 
 SEIR_MODEL_PATH = os.path.join(APP_DIR, "seir_model.R")
 
@@ -73,39 +136,32 @@ class Cache(flask_db.Model):
         return (now - self.timestamp).seconds >= TTL
 
 
+flask_db.database.create_tables(flask_db.Model.__subclasses__())
+
+
 # =============================================================================
 # ROUTE
 # =============================================================================
 
-payload = json.dumps({
-    'Time_to_death': 17,
-    'fact_futuro': 0.5,
-    'D_incbation': 5.2,
-    'D_infectious': 2.9,
-    'R0': 3.422,
-    'R0p': 3.422,
-    'D_recovery_mild': 5.1,
-    'D_recovery_severe': 10.1,
-    'D_hospital_lag': 5,
-    'retardo': 4,
-    'D_death': 14.1,
-    'p_fatal': 0.021,
-    'InterventionTime': 18,
-    'InterventionAmt': 1.0,
-    'p_severe': 0.2,
-    "E0": 17.,
-    'duration': 30,
-    'N': 440000.0,
-    'I0': 1,
-    'timepoints': [0, 1, 2, 3, 4]})
-
-
 @app.route('/')
-def download_csv():
+def index():
+    return render_template("seir.html", dpayload=DEFAULT_PAYLOAD)
+
+
+@app.route('/seir', methods=['POST'])
+def seir():
+
+    payload = json.loads(request.form["query"])
+
+    # it the json is well formed
+    jsonschema.validate(payload, SCHEMA)
+
     now = dt.datetime.now()
 
+    query = json.dumps(payload)
+
     code = hashlib.sha1(
-        str.encode(payload, "utf8")
+        str.encode(query, "utf8")
     ).hexdigest()
 
     cache = Cache.get_or_none(Cache.code == code)
@@ -113,7 +169,7 @@ def download_csv():
     if cache is None:
         cache = Cache(code=code)
     if cache.content is None or cache.expired:
-        cache.content = str(seir_model(payload))
+        cache.content = str(seir_model(query))
         cache.timestamp = now
     cache.save()
 
